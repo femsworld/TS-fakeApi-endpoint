2f3b29074d9213cad78b4591831567dcae56870e
femsworld <femsworld@gmail.com>
2023-05-01 00:07:32 +0300

    Added axios

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios, { Axios, AxiosError } from "axios";
 
-const fakeApi = async (max:number) => {
-    const productResult = await axios("https://api.escuelajs.co/api/v1/products")
+const fakeApi = async (id:number) => {
+    try {
+        const productResult = await axios("https://api.escuelajs.co/api/v1/products")
     const data = productResult.data
-    return data
+       return data
+        
+    } catch (e) {
+    const error = e as AxiosError; //casting the error type
+    console.log(error.message);
+    } 
 }
-console.log(fakeApi)
-
+fakeApi(4).then(data => {
+    console.log(data)
+})
