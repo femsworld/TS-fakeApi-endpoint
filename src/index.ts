073141359fa1c4f1913ddb55e27a71a47f1e8fab
femsworld <femsworld@gmail.com>
2023-05-01 14:24:11 +0300

    Added id as a parameter

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import axios, { Axios, AxiosError } from "axios";
 
 const fakeApi = async (id:number) => {
     try {
-        const productResult = await axios("https://api.escuelajs.co/api/v1/products")
+    //  const productResult = await axios("https://api.escuelajs.co/api/v1/products")
+    const productResult = await axios(`https://api.escuelajs.co/api/v1/products/${id}`); // append the ID to the URL
     const data = productResult.data
-       return data
-        
+       return data 
     } catch (e) {
     const error = e as AxiosError; //casting the error type
     console.log(error.message);
     } 
 }
-fakeApi(4).then(data => {
+fakeApi(104).then(data => {
     console.log(data)
 })
